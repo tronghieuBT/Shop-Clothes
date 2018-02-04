bd840fae2b9e3b93b38de544ccff0afc163f768a
tronghieu_IT\tronghieu <tronghieuhoho@gmail.com>
2018-02-04 17:31:41 +0700

    add *.cs file

---
+++ TeduShop.Web/Mappings/AutoMapperConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using TeduShop.Model.Models;
+using TeduShop.Web.Models;
+
+namespace TeduShop.Web.Mappings
+{
+    public class AutoMapperConfiguration
+    {
+        public static void Configure()
+        {
+            Mapper.CreateMap<Post, PostViewModel>();
+            Mapper.CreateMap<PostCategory, PostCategoryViewModel>();
+            Mapper.CreateMap<Tag, TagViewModel>();
+
+            Mapper.CreateMap<ProductCategory, ProductCategoryViewModel>();
+            Mapper.CreateMap<Product, ProductViewModel>();
+            Mapper.CreateMap<ProductTag, ProductTagViewModel>();
+            Mapper.CreateMap<Footer, FooterViewModel>();
+            Mapper.CreateMap<Slide, SlideViewModel>();
+            Mapper.CreateMap<Page, PageViewModel>();
+            Mapper.CreateMap<ContactDetail, ContactDetailViewModel>();
+
+            Mapper.CreateMap<ApplicationGroup, ApplicationGroupViewModel>();
+            Mapper.CreateMap<ApplicationRole, ApplicationRoleViewModel>();
+            Mapper.CreateMap<ApplicationUser, ApplicationUserViewModel>();
+        }
+
+    }
+}
